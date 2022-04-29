f7f6f6f584f514d30793cf5c1fced75f92db8e90
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:16:54 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,58 @@
+# try:
+#     a = string
+#     b = 12
+#     result = a*b
+# except NameError:
+#     print("bularni qoshib bolmaydi")
+    
+    
+    
+    
+# boshlash = input('boshlashni hohlaysizmi: ')
+# while boshlash == 'ha':
+#     num = int(input('sonni kiriting: '))
+#     num2 = int(input('ikkinchi sonni kiriting: '))
+#     value = input("amalni kiriting: ")
+#     if value == "+":
+#         print("natija: " + str(num + num2))
+#     elif value =="-":
+#         print("natija: " + str(num - num2))
+#     elif value =="*":
+#         print("natija: " + str(num * num2))
+#     elif value =="/":
+#         print("natija: " + str(num / num2))
+#     boshlash = input('qayta boshlash hohlaysizmi: ').lower()
+    
+    
+    
+    
+    
+
+# num = int(input('sonni kiriting: '))
+# num2 = int(input('ikkinchi sonni kiriting: '))
+# value = input("amalni kiriting: ")
+# if value == "+":
+#     print("natija: " + str(num + num2))
+# elif value =="-":
+#     print("natija: " + str(num - num2))
+# elif value =="*":
+#     print("natija: " + str(num * num2))
+# elif value =="/":
+#     print("natija: " + str(num / num2))
+    
+    
+    
+# a = [5, 2, 3, 1, 4]
+# a.sort()
+# print(a)
+    
+    
+
+
+
+num = input('boshlash uchun 1ni bosing: ')
+while num == '1':
+    if num == "salom":
+        print("qales😉")
+    elif num == "qales":
+        print("zor👌")
